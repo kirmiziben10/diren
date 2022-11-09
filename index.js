2fb56737fc6d6902367df650a331bb5479e20c70
Kirmizi Ben10 <76447549+kirmiziben10@users.noreply.github.com>
2022-11-10 01:18:39 +0300

    No more cold commits from now on

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 function transitionToColorSelection() {
-  let nav = document.querySelector("nav");
-  let value = document.querySelector("div#option-value");
-  nav.style.opacity = "0"
+  const nav = document.querySelector("nav");
+  const color = document.querySelector("div#option-color");
+  const value = document.querySelector("div#option-value");
+  const divider = document.querySelector("div#option-divider");
+
+  nav.style.opacity = "0";
   value.style.opacity = "0";
+  divider.style.opacity = "0";
+  color.style.top = "0";
+  color.style.height = "100vh";
+  color.querySelector("h2").style.transform = "translate(0, -23vh)";
 
   setTimeout(() => {
     value.style.display = "none";
@@ -10,10 +17,18 @@ function transitionToColorSelection() {
 }
 
 function transitionToValueSelection() {
-  let nav = document.querySelector("nav");
-  let color = document.querySelector("div#option-color");
-  nav.style.opacity = "0"
+  const nav = document.querySelector("nav");
+  const color = document.querySelector("div#option-color");
+  const value = document.querySelector("div#option-value");
+  const divider = document.querySelector("div#option-divider");
+
+  nav.style.opacity = "0";
   color.style.opacity = "0";
+  divider.style.opacity = "0";
+  value.style.top = "0";
+  value.style.height = "100vh";
+  value.querySelector("h3").transform = "translate(0, -10vh)"
+  value.querySelector("h2").style.transform = "translate(0, -26vh)";
 
   setTimeout(() => {
     color.style.display = "none";
